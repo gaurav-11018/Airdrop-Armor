076b7bcda8889d5527ecf06c12e1b256c22e43bc
Gaurav <gaurav11018@gmail.com>
2023-02-19 20:53:58 +0530

    commit.

--- web/src/App.js
+++ web/src/App.js
@@ -1,179 +1,207 @@
-const { ethers } = require("ethers");
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
-// ABI for the SybilAirdrop contract
+const SybilAirdropAddress = "0x0ba521c07aeee9ef89173e08b3ab23f5e36a8a1d"; // replace with the actual address of the SybilAirdrop contract
 const SybilAirdropABI = [
-  [
-    {
-      inputs: [
-        {
-          internalType: "uint64",
-          name: "requestId",
-          type: "uint64",
-        },
-        {
-          internalType: "address",
-          name: "validator",
-          type: "address",
-        },
-        {
-          internalType: "uint256",
-          name: "schema",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "claimPathKey",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256",
-          name: "operator",
-          type: "uint256",
-        },
-        {
-          internalType: "uint256[]",
-          name: "value",
-          type: "uint256[]",
-        },
-      ],
-      name: "setZKPRequest",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-    {
-      inputs: [
-        {
-          internalType: "uint64",
-          name: "requestId",
-          type: "uint64",
-        },
-        {
-          internalType: "uint256[]",
-          name: "inputs",
-          type: "uint256[]",
-        },
-        {
-          internalType: "uint256[2]",
-          name: "a",
-          type: "uint256[2]",
-        },
-        {
-          internalType: "uint256[2][2]",
-          name: "b",
-          type: "uint256[2][2]",
-        },
-        {
-          internalType: "uint256[2]",
-          name: "c",
-          type: "uint256[2]",
-        },
-      ],
-      name: "submitZKPResponse",
-      outputs: [
-        {
-          internalType: "bool",
-          name: "",
-          type: "bool",
-        },
-      ],
-      stateMutability: "nonpayable",
-      type: "function",
-    },
-  ],
+  {
+    inputs: [
+      {
+        internalType: "uint64",
+        name: "requestId",
+        type: "uint64",
+      },
+      {
+        internalType: "address",
+        name: "validator",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "schema",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "claimPathKey",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "operator",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256[]",
+        name: "value",
+        type: "uint256[]",
+      },
+    ],
+    name: "setZKPRequest",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint64",
+        name: "requestId",
+        type: "uint64",
+      },
+      {
+        internalType: "uint256[]",
+        name: "inputs",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[2]",
+        name: "a",
+        type: "uint256[2]",
+      },
+      {
+        internalType: "uint256[2][2]",
+        name: "b",
+        type: "uint256[2][2]",
+      },
+      {
+        internalType: "uint256[2]",
+        name: "c",
+        type: "uint256[2]",
+      },
+    ],
+    name: "submitZKPResponse",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ];
 
-// Address of the SybilAirdrop contract on the Ethereum network
-const SybilAirdropAddress = "0x0ba521c07aeee9ef89173e08b3ab23f5e36a8a1d";
+function App() {
+  const [account, setAccount] = useState("");
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [sybilAirdropContract, setSybilAirdropContract] = useState(null);
 
-// Address of the ERC20 token contract on the Ethereum network
-const TokenAddress = "0x463c96a29f442c7fac48bc5b7d468f7b5dd37c94";
+  const [amountPerAddress, setAmountPerAddress] = useState(0);
+  const [maxAddresses, setMaxAddresses] = useState(0);
+  const [maxAmount, setMaxAmount] = useState(0);
+  const [totalDistributed, setTotalDistributed] = useState(0);
+  const [totalAddresses, setTotalAddresses] = useState(0);
+  const [claimed, setClaimed] = useState(false);
 
-// Provider for the Ethereum network
-const provider = new ethers.providers.JsonRpcProvider();
+  const [proof, setProof] = useState("");
 
-// Signer for the owner of the SybilAirdrop contract
-const signer = provider.getSigner("0x0ba521c07aeee9ef89173e08b3ab23f5e36a8a1d");
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(web3Provider);
+        const signer = web3Provider.getSigner();
+        setSigner(signer);
+        const account = await signer.getAddress();
+        setAccount(account);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-// Contract instance for the SybilAirdrop contract
-const SybilAirdropContract = new ethers.Contract(
-  SybilAirdropAddress,
-  SybilAirdropABI,
-  provider
-);
+  const claimTokens = async () => {
+    if (signer && sybilAirdropContract && proof) {
+      try {
+        const tx = await sybilAirdropContract.claim(proof);
+        await tx.wait();
+        setClaimed(true);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-// Contract instance for the ERC20 token contract
-const TokenContract = new ethers.Contract(
-  TokenAddress,
-  ["function balanceOf(address) view returns (uint)"],
-  provider
-);
+  useEffect(() => {
+    const getSybilAirdropContract = () => {
+      if (provider) {
+        const sybilAirdropContract = new ethers.Contract(
+          SybilAirdropAddress,
+          SybilAirdropABI,
+          provider
+        );
+        setSybilAirdropContract(sybilAirdropContract);
+      }
+    };
+    getSybilAirdropContract();
+  }, [provider]);
 
-// Function to claim the airdrop for a given Ethereum address
-async function claimAirdrop(address, proof) {
-  // Check if the address has already claimed the airdrop
-  const hasClaimed = await SybilAirdropContract.claimed(address);
-  if (hasClaimed) {
-    console.log("Address has already claimed the airdrop");
-    return;
-  }
+  useEffect(() => {
+    const getSybilAirdropData = async () => {
+      if (sybilAirdropContract) {
+        try {
+          const amountPerAddress =
+            await sybilAirdropContract.amountPerAddress();
+          setAmountPerAddress(amountPerAddress);
+          const maxAddresses = await sybilAirdropContract.maxAddresses();
+          setMaxAddresses(maxAddresses);
+          const maxAmount = await sybilAirdropContract.maxAmount();
+          setMaxAmount(maxAmount);
+          const totalDistributed =
+            await sybilAirdropContract.totalDistributed();
+          setTotalDistributed(totalDistributed);
+          const totalAddresses = await sybilAirdropContract.totalAddresses();
+          setTotalAddresses(totalAddresses);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+    getSybilAirdropData();
+  }, [sybilAirdropContract]);
 
-  // Get the amount of tokens to be distributed per address
-  const amountPerAddress = await SybilAirdropContract.amountPerAddress();
-
-  // Get the ZKP request ID for the SybilAirdrop contract
-  const zkpRequestId = await SybilAirdropContract.zkpRequestId();
-
-  // Compute the hash of the message to be signed
-  const hash = ethers.utils.solidityKeccak256(
-    ["address", "uint256", "uint64"],
-    [address, amountPerAddress, zkpRequestId]
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sybil Airdrop Claim</h1>
+      </header>
+      <main>
+        {account ? (
+          <>
+            <p>Connected account: {account}</p>
+            <p>Amount per address: {amountPerAddress.toString()}</p>
+            <p>Maximum number of addresses: {maxAddresses.toString()}</p>
+            <p>Maximum amount to be distributed: {maxAmount.toString()}</p>
+            <p>Total distributed: {totalDistributed.toString()}</p>
+            <p>Total number of addresses: {totalAddresses.toString()}</p>
+            {claimed ? (
+              <p>Tokens claimed!</p>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  value={proof}
+                  onChange={(e) => setProof(e.target.value)}
+                />
+                <button onClick={claimTokens}>Claim tokens</button>
+              </>
+            )}
+          </>
+        ) : (
+          <button onClick={connectWallet}>Connect wallet</button>
+        )}
+      </main>
+    </div>
   );
-
-  // Verify the signature of the message
-  const signer = await ethers.utils.verifyMessage(
-    ethers.utils.arrayify(hash),
-    proof
-  );
-  if (signer !== "0x1234567890123456789012345678901234567890") {
-    console.log("Invalid signature");
-    return;
-  }
-
-  // Submit the ZKP response to the SybilAirdrop contract
-  const inputs = [address, amountPerAddress, zkpRequestId];
-  const a = [0, 0];
-  const b = [
-    [0, 0],
-    [0, 0],
-  ];
-  const c = [0, 0];
-
-  const response = await SybilAirdropContract.submitZKPResponse(
-    inputs,
-    a,
-    b,
-    c
-  );
-  if (!response) {
-    console.log("Failed to submit ZKP response");
-    return;
-  }
-
-  // Transfer tokens to the address
-  const tokenBalance = await TokenContract.balanceOf(SybilAirdropAddress);
-  const amount = Math.min(tokenBalance, amountPerAddress);
-  const transferResponse = await TokenContract.transfer(address, amount);
-  if (!transferResponse) {
-    console.log("Failed to transfer tokens to address");
-    return;
-  }
-
-  console.log(`Transferred ${amount} tokens to address ${address}`);
 }
+
+export default App;
